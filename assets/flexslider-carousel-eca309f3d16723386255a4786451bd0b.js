81773218a4bd0bc891e694a4f7473b44e62fc1a4
Alex Gonzalez <alex@lindusembedded.com>
2014-08-22 00:17:00 +0200

    v3
    Signed-off-by: Alex Gonzalez <alex@lindusembedded.com>

---
+++ assets/flexslider-carousel-eca309f3d16723386255a4786451bd0b.js
@@ -0,0 +1,11 @@
+// Initialise FlexSlider
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemWidth: 210,
+    itemMargin: 5,
+    controlNav: false,
+    slideshow: false
+  });
+});
